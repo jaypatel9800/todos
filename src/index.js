3e9516b9022561d1e35065d650e37602b207ae81
Jay Patel <jaypatel9800@gmail.com>
2021-07-09 18:30:48 +0530

    adding history

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React, {StrictMode} from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import { HashRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
+const history = createBrowserHistory();
 
 ReactDOM.render(
 <StrictMode>    
-<Router basename="/">
+<Router history={history}>
 <App/>
 </Router>
 </StrictMode>, document.getElementById("root"))
