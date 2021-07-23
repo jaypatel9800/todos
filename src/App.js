31520af8946c156286b8472b185dcb4a8e7b8e63
Jay Patel <jaypatel9800@gmail.com>
2021-07-23 22:08:23 +0530

    add authername and fix bugs.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Diff from "./bottom";
-import sun from "./images/icon-sun.svg"
-import moon from "./images/icon-moon.svg"
+import sun from "./images/icon-sun.svg";
+import moon from "./images/icon-moon.svg";
 import cross from "./images/icon-cross.svg";
 import { Route } from "react-router-dom";
 import "./style.css";
@@ -13,24 +13,24 @@ const App = () => {
   const [comp, addComp] = useState([]);
   const [count, changeCount] = useState("");
   const [mark, changeMark] = useState(false);
-  const [darkTheme, setDarkTheme] = useState(getDefaultTheme())
+  const [darkTheme, setDarkTheme] = useState(getDefaultTheme());
 
   useEffect(() => {
     const addCount = () => {
       const a = item.map((a) => a.complete);
       const b = a.filter((a) => !a);
       changeCount(b.length);
-      if(b.length !== 0){
-        changeMark(false)
+      if (b.length !== 0) {
+        changeMark(false);
       }
     };
-    localStorage.setItem('dark', JSON.stringify(darkTheme))
+    localStorage.setItem("dark", JSON.stringify(darkTheme));
     addCount();
   });
 
   function getDefaultTheme() {
-    const selectedTheme = JSON.parse(localStorage.getItem('dark'))
-    return selectedTheme || false
+    const selectedTheme = JSON.parse(localStorage.getItem("dark"));
+    return selectedTheme || false;
   }
 
   const add = () => {
@@ -70,8 +70,8 @@ const App = () => {
     addComp(news3);
   };
   const styles = {
-    textDecoration : "line-through",
-    opacity: 0.5
+    textDecoration: "line-through",
+    opacity: 0.5,
   };
 
   const activeItem = () => {
@@ -96,12 +96,12 @@ const App = () => {
     const old = [...item];
     const danger = old.filter((a) => !a.complete);
     addItem(danger);
-    changeMark(false)
+    changeMark(false);
   };
 
   const markedAll = () => {
     const old = [...item];
-    if(mark === false){
+    if (mark === false) {
       const allMark = old.filter((a) => {
         if (a.complete === false) {
           a.complete = !a.complete;
@@ -109,8 +109,8 @@ const App = () => {
         }
         return a;
       });
-      addItem(allMark)
-    }else{
+      addItem(allMark);
+    } else {
       const allUnMark = old.filter((a) => {
         if (a.complete === true) {
           a.complete = !a.complete;
@@ -118,105 +118,113 @@ const App = () => {
         changeMark(false);
         return a;
       });
-      addItem(allUnMark)
+      addItem(allUnMark);
     }
   };
   return (
-      <div className={darkTheme ? 'dark' : 'light'}>
-        <section>
+    <div className={darkTheme ? "dark" : "light"}>
+      <section>
         <div className="header">
-            <h1>TODO</h1>
-            <button onClick={()=> setDarkTheme(prevTheme => !prevTheme)} style={{cursor:"pointer"}}>
-                <img src={darkTheme ? sun : moon} alt="moon/sun"/>
-            </button>
+          <h1>TODO</h1>
+          <button
+            onClick={() => setDarkTheme((prevTheme) => !prevTheme)}
+            style={{ cursor: "pointer" }}
+          >
+            <img src={darkTheme ? sun : moon} alt="moon/sun" />
+          </button>
         </div>
-          <div className="mainInp">
-            <div>
-              <input
-                type="checkbox"
-                checked={mark}
-                onClick={markedAll}
-                className="checkbox"
-                style={{cursor:"pointer"}}
-              />
-            </div>
+        <div className="mainInp">
+          <div>
             <input
-              className="write"
-              value={text}
-              onChange={(e) => changetext(e.target.value)}
-              onKeyPress={addText}
-              placeholder="Create a new todo"
+              type="checkbox"
+              checked={mark}
+              onClick={markedAll}
+              className="checkbox"
+              style={{ cursor: "pointer" }}
             />
           </div>
-          <Route path="/" exact>
-            <ul>
-              {item.map((a) => (
-                <li key={a.id}>
-                  <div>
-                    <input
-                      type="checkbox"
-                      checked={a.complete}
-                      onChange={() => marked(a.id)}
-                      className="checkbox"
-                      style={{cursor:"pointer"}}
-                    />
-                    <h2 style={a.complete ? styles : null}>{a.text}</h2>
-                  </div>
+          <input
+            className="write"
+            value={text}
+            onChange={(e) => changetext(e.target.value)}
+            onKeyPress={addText}
+            placeholder="Create a new todo"
+          />
+        </div>
+        <Route path="/" exact>
+          <ul>
+            {item.map((a) => (
+              <li key={a.id}>
+                <div>
+                  <input
+                    type="checkbox"
+                    checked={a.complete}
+                    onChange={() => marked(a.id)}
+                    className="checkbox"
+                    style={{ cursor: "pointer" }}
+                  />
+                  <h2 style={a.complete ? styles : null}>{a.text}</h2>
+                </div>
 
-                  <button onClick={() => clear(a.id)}>
-                    <img src={cross} alt="cross" />
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </Route>
-          <Route path="/active">
-            <ul>
-              {active.map((a) => (
-                <li key={a.id}>
-                  <div>
-                    <input
-                      type="checkbox"
-                      checked={a.complete}
-                      onChange={() => marked(a.id)}
-                      className="checkbox"
-                    />
-                    <h2 style={a.complete ? styles : null}>{a.text}</h2>
-                  </div>
-                  <button onClick={() => clear(a.id)}>
-                    <img src={cross} alt="cross" />
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </Route>
-          <Route path="/complete">
-            <ul>
-              {comp.map((a) => (
-                <li key={a.id}>
-                  <div>
-                    <input
-                      type="checkbox"
-                      checked={a.complete}
-                      onChange={() => marked(a.id)}
-                      className="checkbox"
-                    />
-                    <h2 style={a.complete ? styles : null}>{a.text}</h2>
-                  </div>
-                  <button onClick={() => clear(a.id)}>
-                    <img src={cross} alt="cross" />
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </Route>
-          <div className="bottom">
-            <p>{count} items left</p>
-            <Diff activeItem={activeItem} cmpltItem={cmpltItem} add={add} />
-            <button onClick={danger} style={{cursor:"pointer"}}>Clear Completed</button>
-          </div>
-        </section>
+                <button onClick={() => clear(a.id)}>
+                  <img src={cross} alt="cross" />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </Route>
+        <Route path="/active">
+          <ul>
+            {active.map((a) => (
+              <li key={a.id}>
+                <div>
+                  <input
+                    type="checkbox"
+                    checked={a.complete}
+                    onChange={() => marked(a.id)}
+                    className="checkbox"
+                  />
+                  <h2 style={a.complete ? styles : null}>{a.text}</h2>
+                </div>
+                <button onClick={() => clear(a.id)}>
+                  <img src={cross} alt="cross" />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </Route>
+        <Route path="/complete">
+          <ul>
+            {comp.map((a) => (
+              <li key={a.id}>
+                <div>
+                  <input
+                    type="checkbox"
+                    checked={a.complete}
+                    onChange={() => marked(a.id)}
+                    className="checkbox"
+                  />
+                  <h2 style={a.complete ? styles : null}>{a.text}</h2>
+                </div>
+                <button onClick={() => clear(a.id)}>
+                  <img src={cross} alt="cross" />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </Route>
+        <div className="bottom">
+          <p>{count} items left</p>
+          <Diff activeItem={activeItem} cmpltItem={cmpltItem} add={add} />
+          <button onClick={danger} style={{ cursor: "pointer" }}>
+            Clear Completed
+          </button>
+        </div>
+      </section>
+      <div className="names">
+      <h3 className="lowerName">Challenged by <a href="https://www.frontendmentor.io/" target="_blank" rel="noopener noreferrer">Frontendmentor</a>  |  Completed by <a href="https://jaypatel.cf/" target="_blank" rel="noopener noreferrer">Jay Patel</a></h3>
       </div>
+    </div>
   );
 };
 
